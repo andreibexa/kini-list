a8ad3423801fdb0f3f22f17b3e787c30708953fd
Andrei Bexa <andreibexa@gmail.com>
2022-04-07 03:45:28 +0300

    Lazy load routes

--- src/routes/pageLinks.ts
+++ src/routes/pageLinks.ts
@@ -2,39 +2,40 @@ import HomeIcon from '@mui/icons-material/Home';
 /* import LineStyleIcon from '@mui/icons-material/LineStyle';
 import LocalMoviesOutlinedIcon from '@mui/icons-material/LocalMoviesOutlined';
 import MovieIcon from '@mui/icons-material/Movie'; */
-import React from 'react';
-import Home from 'pages/Home/Home';
-import { IPageLinks } from '../types/pageLinks';
+import React, {lazy} from 'react';
+import {IPageLinks} from '../types/pageLinks';
+
+const Home = lazy(() => import('pages/Home/Home'));
 
 const pageLinks: IPageLinks[] = [
-  {
-    component: Home,
-    label: 'Home',
-    icon: React.createElement('<>', {}, HomeIcon),
-    index: true,
-    path: '/',
-  },
-  /* {
-    label: 'Movies',
-    component: React.createElement('<>', {}, Home()),
-    index: false,
-    path: '/',
-    icon: React.createElement('<>', {}, MovieIcon),
-  },
-  {
-    label: 'Series',
-    component: React.createElement('<>', {}, Home()),
-    index: false,
-    path: '/',
-    icon: React.createElement('<>', {}, LocalMoviesOutlinedIcon),
-  },
-  {
-    label: 'More ...',
-    component: React.createElement('<>', {}, Home()),
-    index: false,
-    path: '/',
-    icon: React.createElement('<>', {}, LineStyleIcon),
-  }, */
+    {
+        component: Home,
+        label: 'Home',
+        icon: React.createElement('<>', {}, HomeIcon),
+        index: true,
+        path: '/',
+    },
+    /* {
+      label: 'Movies',
+      component: React.createElement('<>', {}, Home()),
+      index: false,
+      path: '/',
+      icon: React.createElement('<>', {}, MovieIcon),
+    },
+    {
+      label: 'Series',
+      component: React.createElement('<>', {}, Home()),
+      index: false,
+      path: '/',
+      icon: React.createElement('<>', {}, LocalMoviesOutlinedIcon),
+    },
+    {
+      label: 'More ...',
+      component: React.createElement('<>', {}, Home()),
+      index: false,
+      path: '/',
+      icon: React.createElement('<>', {}, LineStyleIcon),
+    }, */
 ];
 
 export default pageLinks;
