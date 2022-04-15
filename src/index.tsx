8a05c54e272449289a951a1a2658697a7cb1d297
Andrei Bexa <andreibexa@gmail.com>
2022-04-16 01:16:40 +0300

    Revert "Create main.yml"
    This reverts commit 258d308093f1da7d9446498cd72c336910220766.

--- src/index.tsx
+++ src/index.tsx
@@ -16,7 +16,7 @@ const root = createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
